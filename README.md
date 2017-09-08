cceebb8787bd74cfe4df325e2e942ed9bd63ea78
cvgellhorn <gellborn@gmail.com>
2017-09-09 01:27:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+mail-redactor
+===========
+> Fool all the evil mail harvesting bots
+
+# Usage
+``` js
+const mailRedactor = require('mail-redactor');
+console.log(mailRedactor('contact@website.com'));
+```
+# Output
+``` js
+co
+<span>n</span>
+<span>t</span>
+<span>a</span>
+c
+<span>t</span>
+<span>@</span>
+we
+<span>b</span>
+s
+<span>i</span>
+te
+<span>.</span>
+<span>c</span>
+o
+<span>m</span>
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Redact given mail to fool all the evil harvesting bots
+ *
+ * @param {string} mail
+ * @returns {string}
+ */
+module.export = function(mail) {
+    const mailLetters = mail.split('');
+
+    for (let i = 0; i < mailLetters.length; i++) {
+        if (Math.floor(Math.random() * 10) % 2) {
+            mailLetters[i] = `<span>${mailLetters[i]}</span>`;
+        }
+    }
+
+    return mailLetters.join('');
+};
