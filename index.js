a86080856b21ed03fdf5bf1e39deeec609477d06
Aleksandar Babic <alekski64@icloud.com>
2018-05-17 00:34:03 +0200

    Improved mail string length check

--- index.js
+++ index.js
@@ -6,8 +6,9 @@
  */
 module.exports = function(mail) {
     const mailLetters = mail.split('');
+    const mailLettersLength = mailLetters.length;
 
-    for (let i = 0; i < mailLetters.length; i++) {
+    for (let i = 0; i < mailLettersLength; i++) {
         if (Math.floor(Math.random() * 10) % 2) {
             mailLetters[i] = `<span>${mailLetters[i]}</span>`;
         }
